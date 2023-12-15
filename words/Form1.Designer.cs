feebec77ef3a2f3e977903aefbf1d2c11cfb8557
kingcoder <mangawilliam401@gmail.com>
2023-12-15 13:17:55 +0000

    hii guys

--- words/Form1.Designer.cs
+++ words/Form1.Designer.cs
@@ -28,70 +28,208 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // richTextBox1
+            // button1
             // 
-            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox1.Location = new System.Drawing.Point(121, 338);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(67, 49);
-            this.richTextBox1.TabIndex = 2;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.ForeColor = System.Drawing.Color.Cyan;
+            this.button1.Location = new System.Drawing.Point(54, 338);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 49);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // richTextBox2
+            // button2
             // 
-            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox2.Location = new System.Drawing.Point(267, 338);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(67, 49);
-            this.richTextBox2.TabIndex = 3;
-            this.richTextBox2.Text = "";
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.ForeColor = System.Drawing.Color.Cyan;
+            this.button2.Location = new System.Drawing.Point(111, 338);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(51, 49);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // richTextBox3
+            // button3
             // 
-            this.richTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox3.Location = new System.Drawing.Point(194, 338);
-            this.richTextBox3.Name = "richTextBox3";
-            this.richTextBox3.Size = new System.Drawing.Size(67, 49);
-            this.richTextBox3.TabIndex = 4;
-            this.richTextBox3.Text = "";
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.ForeColor = System.Drawing.Color.Cyan;
+            this.button3.Location = new System.Drawing.Point(168, 338);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(51, 49);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // richTextBox4
+            // button4
             // 
-            this.richTextBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox4.Location = new System.Drawing.Point(48, 338);
-            this.richTextBox4.Name = "richTextBox4";
-            this.richTextBox4.Size = new System.Drawing.Size(67, 49);
-            this.richTextBox4.TabIndex = 5;
-            this.richTextBox4.Text = "";
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.ForeColor = System.Drawing.Color.Cyan;
+            this.button4.Location = new System.Drawing.Point(225, 338);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(51, 49);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Black;
+            this.button5.ForeColor = System.Drawing.Color.Cyan;
+            this.button5.Location = new System.Drawing.Point(282, 338);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(51, 49);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Black;
+            this.button6.ForeColor = System.Drawing.Color.Cyan;
+            this.button6.Location = new System.Drawing.Point(339, 338);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(51, 49);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(54, 124);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(51, 38);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(111, 124);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(51, 38);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(168, 124);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(51, 38);
+            this.button9.TabIndex = 15;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(225, 124);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(51, 38);
+            this.button10.TabIndex = 16;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(282, 124);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(51, 38);
+            this.button11.TabIndex = 17;
+            this.button11.Text = "button11";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(339, 124);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(51, 38);
+            this.button12.TabIndex = 18;
+            this.button12.Text = "button12";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(645, 64);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 30);
+            this.button13.TabIndex = 19;
+            this.button13.Text = "button13";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(594, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "WORD:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 460);
-            this.Controls.Add(this.richTextBox4);
-            this.Controls.Add(this.richTextBox3);
-            this.Controls.Add(this.richTextBox2);
-            this.Controls.Add(this.richTextBox1);
+            this.ClientSize = new System.Drawing.Size(818, 460);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.RichTextBox richTextBox2;
-        private System.Windows.Forms.RichTextBox richTextBox3;
-        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- words/Form1.cs
+++ words/Form1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,12 @@ namespace words
 {
     public partial class Form1 : Form
     {
+        List<string> words = new List<string>();
+        
+        
+        string combine = "";
+        char[] chars = new char[8];
+        int top = 0;
         string s = "";
         public Form1()
         {
@@ -20,6 +28,23 @@ namespace words
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            List<String> wordlist = new List<String>();
+            StreamReader file = new StreamReader("../../resources/ukenglish.txt");
+            {
+                string ln;
+                while ((ln = file.ReadLine()) != null)
+                {
+                    wordlist.Add(ln);
+                }
+                file.Close();
+            }
+            for(int i = 0; i < wordlist.Count;i++)
+            {
+                words.Add(wordlist[i]);
+            }
+            
+            
+
             Random rnd = new Random();
             char[] s = new char[4];
             for (int i = 0; i < 4; i++)
@@ -28,38 +53,311 @@ namespace words
                 string letters = "BCDFGHGKLMNPQRSTVWXYZ";
                 s[i] = letters[letter];
             }
-            richTextBox1.Text = Convert.ToString(s[0]);
-            richTextBox2.Text = Convert.ToString(s[1]);
-            richTextBox3.Text = Convert.ToString(s[2]);
-            richTextBox4.Text = Convert.ToString(s[3]);
+            button1.Text = Convert.ToString(s[0]);
+            button2.Text = Convert.ToString(s[1]);
+            button3.Text = Convert.ToString(s[2]);
+            button4.Text = Convert.ToString(s[3]);
 
             char[] vowel = new char[2];
             for (int i = 0; i < 2; i++)
             {
                 string v = "AEIOU";
                 int vowels = rnd.Next(0, 4);
-
-
+                vowel[i] = v[vowels];
             }
+            button5.Text = Convert.ToString(vowel[0]);
+            button6.Text = Convert.ToString(vowel[1]);
+
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+            button10.Text = "";
+            button11.Text = "";
+            button12.Text = "";
+            button13.Text = "";
+            
+
+
+
 
 
 
         }
         static void randomletters()
         {
-           
-  
+
+
         }
-       
+
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            
+
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (top == 0)
+            {
+                button7.Text = button1.Text;
+            }
+            else if (top == 1)
+            {
+                button8.Text = button1.Text;
+            }
+            else if (top == 2)
+            {
+                button9.Text = button1.Text;
+            }
+            else if (top == 3)
+            {
+                button10.Text = button1.Text;
+            }
+            else if (top == 4)
+            {
+                button11.Text = button1.Text;
+            }
+            else if (top == 5)
+            {
+                button12.Text = button1.Text;
+                
+            }
+            chars[top] = Convert.ToChar(button1.Text);
+            top = top + 1;
+            if (top == 6)
+            {
+                combine = Convert.ToString(chars[0]) + Convert.ToString(chars[1]) + Convert.ToString(chars[2]) + Convert.ToString(chars[3]) + Convert.ToString(chars[4]) + Convert.ToString(chars[5]);
+                button13.Text = combine;
+                score(combine, words);
+            }
             
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (top == 0)
+            {
+                button7.Text = button2.Text;
+            }
+            else if (top == 1)
+            {
+                button8.Text = button2.Text;
+            }
+            else if (top == 2)
+            {
+                button9.Text = button2.Text;
+            }
+            else if (top == 3)
+            {
+                button10.Text = button2.Text;
+            }
+            else if (top == 4)
+            {
+                button11.Text = button2.Text;
+            }
+            else if (top == 5)
+            {
+                button12.Text = button2.Text;
+                combine = Convert.ToString(chars[0]) + Convert.ToString(chars[1]) + Convert.ToString(chars[2]) + Convert.ToString(chars[3]) + Convert.ToString(chars[4]) + Convert.ToString(chars[5]);
+                button13.Text = combine;
+            }
+            chars[top] = Convert.ToChar(button2.Text);
+            top = top + 1;
+            if (top == 6)
+            {
+                combine = Convert.ToString(chars[0]) + Convert.ToString(chars[1]) + Convert.ToString(chars[2]) + Convert.ToString(chars[3]) + Convert.ToString(chars[4]) + Convert.ToString(chars[5]);
+                button13.Text = combine;
+                score(combine, words);
+            }
+            
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (top == 0)
+            {
+                button7.Text = button3.Text;
+            }
+            else if (top == 1)
+            {
+                button8.Text = button3.Text;
+            }
+            else if (top == 2)
+            {
+                button9.Text = button3.Text;
+            }
+            else if (top == 3)
+            {
+                button10.Text = button3.Text;
+            }
+            else if (top == 4)
+            {
+                button11.Text = button3.Text;
+            }
+            else if (top == 5)
+            {
+                button12.Text = button3.Text;
+            }
+            chars[top] = Convert.ToChar(button3.Text);
+            top = top + 1;
+            if (top == 6)
+            {
+                combine = Convert.ToString(chars[0]) + Convert.ToString(chars[1]) + Convert.ToString(chars[2]) + Convert.ToString(chars[3]) + Convert.ToString(chars[4]) + Convert.ToString(chars[5]);
+                button13.Text = combine;
+                score(combine, words);
+            }
+            
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (top == 0)
+            {
+                button7.Text = button4.Text;
+            }
+            else if (top == 1)
+            {
+                button8.Text = button4.Text;
+            }
+            else if (top == 2)
+            {
+                button9.Text = button4.Text;
+            }
+            else if (top == 3)
+            {
+                button10.Text = button4.Text;
+            }
+            else if (top == 4)
+            {
+                button11.Text = button4.Text;
+            }
+            else if (top == 5)
+            {
+                button12.Text = button4.Text;
+
+            }
+            chars[top] = Convert.ToChar(button4.Text);
+            top = top + 1;
+            if (top == 6)
+            {
+                combine = Convert.ToString(chars[0]) + Convert.ToString(chars[1]) + Convert.ToString(chars[2]) + Convert.ToString(chars[3]) + Convert.ToString(chars[4]) + Convert.ToString(chars[5]);
+                button13.Text = combine;
+                score(combine, words);
+            }
+            
+
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (top == 0)
+            {
+                button7.Text = button5.Text;
+            }
+            else if (top == 1)
+            {
+                button8.Text = button5.Text;
+            }
+            else if (top == 2)
+            {
+                button9.Text = button5.Text;
+            }
+            else if (top == 3)
+            {
+                button10.Text = button5.Text;
+            }
+            else if (top == 4)
+            {
+                button11.Text = button5.Text;
+            }
+            else if (top == 5)
+            {
+                button12.Text = button5.Text;
+                score(combine, words);
+
+            }
+            chars[top] = Convert.ToChar(button5.Text);
+            top = top + 1;
+            if (top == 6)
+            {
+                combine = Convert.ToString(chars[0]) + Convert.ToString(chars[1]) + Convert.ToString(chars[2]) + Convert.ToString(chars[3]) + Convert.ToString(chars[4]) + Convert.ToString(chars[5]);
+                button13.Text = combine;
+            }
+            
+
+
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (top == 0)
+            {
+                button7.Text = button6.Text;
+            }
+            else if (top == 1)
+            {
+                button8.Text = button6.Text;
+            }
+            else if (top == 2)
+            {
+                button9.Text = button6.Text;
+            }
+            else if (top == 3)
+            {
+                button10.Text = button6.Text;
+            }
+            else if (top == 4)
+            {
+                button11.Text = button6.Text;
+            }
+            else if (top == 5)
+            {
+                button12.Text = button6.Text;
+  
+            }
+            chars[top] = Convert.ToChar(button6.Text);
+            top = top + 1;
+            if (top == 6)
+            {
+                combine = Convert.ToString(chars[0]) + Convert.ToString(chars[1]) + Convert.ToString(chars[2]) + Convert.ToString(chars[3]) + Convert.ToString(chars[4]) + Convert.ToString(chars[5]);
+                button13.Text = combine;
+                score(combine, words);
+            }
+            
+
+
+
+        }
+        public static void score(string combine,List<string> words)
+        {
+            for(int i = 0; i < words.Count;i++)
+            {
+                if (words[i] == combine)
+                {
+                    MessageBox.Show("well done");
+                    break;
+                }
+                else
+                {
+                    MessageBox.Show("wrong");
+                    break;
+                }
+            }
         }
     }
 }
